ada40af18bee723bf7a04fd9703c44b0ba47b4e4
Kranberry <60517786+Kranberry@users.noreply.github.com>
2021-01-16 00:16:00 +0100

    Edited a comment so it was more clear

--- bin/Debug/net5.0/scripts/test.cs
+++ bin/Debug/net5.0/scripts/test.cs
@@ -1,10 +1,11 @@
-void Lmao()
+void TestMethod()
 {
     Console.WriteLine("Hello from inside the method!");
 }
 
 List<string> lista = new();
 lista.Add("Hello from inside the lista");
+
 Console.WriteLine("Hello from inside the file!");
-Lmao();
+TestMethod();
 Console.WriteLine(lista[0]);
